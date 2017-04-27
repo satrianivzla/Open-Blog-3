3bcac278340e29556476bdba19a4bb49ed7924b8
Simon <simon_montano@yahoo.com>
2017-04-27 16:21:43 -0400

    Update sidebar.php
    updated to create link on footer and in the Top Bar

--- application/themes/default_admin/partials/sidebar.php
+++ application/themes/default_admin/partials/sidebar.php
@@ -3,8 +3,7 @@
             <?php foreach ($this->template->admin_nav as $nav): ?>
             <li class="<?php echo ($this->template->active_link == $nav['name'])? "active": ''; ?>"><?= $nav['link'] ?></li>
             <?php endforeach ?>
-            <li><a href="<?= site_url() ?>" target="_blank">View Site</a></li>
+ 
           </ul>
-
-          <p><small>Powered by <a href="http://open-blog.org">Open Blog</a></small></p>
+ 
         </div>
