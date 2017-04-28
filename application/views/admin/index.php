6e6542190912de576b9e7cd42184c4f7b6a0cbbf
Simon <simon_montano@yahoo.com>
2017-04-27 22:28:34 -0400

    Update index.php

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,68 +1,81 @@
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title"><?php echo lang('dashboard_title'); ?></h3>
+  </div>
+  <div class="panel-body">
+  
+  
 <div class="row">
     <div class="col-md-3 col-sm-6 col-xs-6"> 
         <div class="panel status panel-success">
             <div class="panel-heading">
-                <h1 class="panel-title text-center"><?= $post_count ?></h1>
+                <h1 class="panel-title text-center"><?php echo $post_count; ?></h1>
             </div>
             <div class="panel-body text-center">                        
-                <strong>Published Posts</strong>
+                <strong><?php echo lang('dashboard_box_published'); ?></strong>
             </div>
         </div>          
     </div>
     <div class="col-md-3 col-sm-6 col-xs-6"> 
         <div class="panel status panel-success">
             <div class="panel-heading">
-                <h1 class="panel-title text-center"><?= $active_comments_count ?></h1>
+                <h1 class="panel-title text-center"><?php echo $active_comments_count; ?></h1>
             </div>
             <div class="panel-body text-center">                        
-                <strong>Active Comments</strong>
+                <strong><?php echo lang('dashboard_box_comments'); ?></strong>
             </div>
         </div>  
     </div>
     <div class="col-md-3 col-sm-6 col-xs-6"> 
         <div class="panel status panel-danger">
             <div class="panel-heading">
-                <h1 class="panel-title text-center"><?= $modded_comments_count ?></h1>
+                <h1 class="panel-title text-center"><?php echo $modded_comments_count; ?></h1>
             </div>
             <div class="panel-body text-center">                        
-                <strong>Comments Awaiting Moderation</strong>
+                <strong><?php echo lang('dashboard_box_moderation'); ?></strong>
             </div>
         </div>  
     </div>
     <div class="col-md-3 col-sm-6 col-xs-6"> 
         <div class="panel status panel-warning">
             <div class="panel-heading">
-                <h1 class="panel-title text-center"><?= $notification_count ?></h1>
+                <h1 class="panel-title text-center"><?php echo $notification_count; ?></h1>
             </div>
             <div class="panel-body text-center">                        
-                <strong>Subscriptions to New Content</strong>
+                <strong><?php echo lang('dashboard_box_subscriptions'); ?></strong>
             </div>
         </div>  
     </div>
 </div>
 
-
-
-
-
 <!-- last OB News -->
 <div class="row">
-    <div class="colxs-12">
-        <h2>Open Blog News</h2>
+    <div class="col-xs-12">
+        <h2 class="page-header"><?php echo lang('openblog_news_title'); ?></h2>
         <?php if ($news): ?>
             <?php foreach ($news as $item): ?>
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="panel-title"><?= $item->title ?> <span class="pull-right"><?= $item->date_posted ?></span></h3>
+                    <h3 class="panel-title"><?php echo $item->title; ?> <span class="pull-right"><?php echo $item->date_posted; ?></span></h3>
                 </div>
                 <div class="panel-body">
-                    <?= $item->excerpt ?> <br><span class="pull-right"><a href="http://open-blog.org" target="_blank">Visit the Open Blog Website for more Help and News.</a></span>
+                    <?php echo $item->excerpt; ?> <br><span class="pull-right"><a href="http://open-blog.org" target="_blank"></a><?php echo lang('openblog_message'); ?></span>
                 </div>
             </div>
             <?php endforeach ?>
 
         <?php else: ?>
-        <h3 class="text-center">No News Found</h3>
+		
+		   <div class="alert alert-info alert-dismissable">
+			  <button type="button" class="close" data-dismiss="alert">&times;</button>
+			  <strong><?php echo lang('error_title_important'); ?></strong> <br />
+			  <?php echo lang('error_no_openblog_news'); ?>
+			</div>
+
         <?php endif ?>
     </div>
+</div>
+  
+  
+  </div>
 </div>
