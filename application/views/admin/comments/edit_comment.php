6cbbae68005f3914d2ce99089d5fb6b3106c0a5b
Simon <simon_montano@yahoo.com>
2017-05-27 16:27:35 -0400

    Add files via upload

--- application/views/admin/comments/edit_comment.php
+++ application/views/admin/comments/edit_comment.php
@@ -1,42 +1,80 @@
+<?php 
+ /**
+ * Edit Comments
+ * 
+ * @access       public
+ * @author       Enliven Appications
+ * @version      3.0
+ * Last Updated  May 04, 2017
+ * used bootstrap panels, breadcrumbs and alerts; Changed way to show validation messages and also added some language variables 
+ * @author       Simon Montaño
+*/
+?>
+
+        <div class="btn-group btn-breadcrumb">
+            <a href="<?php echo site_url('admin'); ?>" class="btn btn-default"><i class="glyphicon glyphicon-home"></i></a>
+            <a href="<?php echo site_url('admin_comments'); ?>" class="btn btn-default"><?php echo lang('comments_section_name'); ?></a>
+			<a href="#" class="btn btn-primary"><?php echo lang('comments_edit_page'); ?></a>
+         </div>		 
+		 <br /> &nbsp; <br /> 
+		 		 
 <div class="row">
 	<div class="col-xs-12">
-		<?= validation_errors() ?>
+        <?php 
+		   if (validation_errors()) {  ?>
+			<div class="alert alert-danger alert-dismissable">
+			  <button type="button" class="close" data-dismiss="alert">&times;</button>
+			  <strong><?php echo lang('error_title_important'); ?></strong> <br /> 
+			   <?php echo lang('error_general_message'); ?>
+ 			</div>
+         <?php } ?>
 
-		<?php if (isset($message)): ?>
-			<div class="alert alert-danger" role="alert">
-				<?= $message ?>
+			<?php if (isset($message)): ?>
+				<div class="alert alert-danger" role="alert">
+					<?php echo $message; ?>
+				</div>
+			<?php endif ?>
+	</div>
+</div>
+		
+			<div class="panel panel-default">
+			  <div class="panel-heading">
+				<h3 class="panel-title"><?php echo lang('comment_edit_hdr'); ?></h3>
+			  </div>
+			  <div class="panel-body">
+				 		<?php echo form_open(current_url()); ?>
+		<p><?php echo lang('comment_edit_subheading'); ?></p>		
+		<div class="row">
+
+			<div class="col-xs-8">
+				<div class="form-group">
+					<label for="content"><?php echo lang('comment_form_field_content'); ?></label>
+					<div class="well help-block"><?php echo lang('comment_form_field_content_hlp_txt'); ?></div>
+					<?php echo form_textarea(['name' => 'content', 'id' => 'content', 'class' => 'form-control', 'value' => $comment['content'], 'placeholder' => lang('comment_form_field_content')]); ?>
+				</div>
+
+				<div class="form-group">
+					<input class="btn btn-lg btn-default btn-block" type="submit" value="<?php echo lang('comments_save_btn');?>">
+				</div>
 			</div>
-		<?php endif ?>
 
-		<h2><?= lang('comment_edit_hdr');?></h2>
-		<?= form_open(current_url());?>
-		<p><?= lang('comment_edit_subheading');?></p>
-	</div>
-</div>
+			<div class="col-xs-4">
 			
-<div class="row">
-
-	<div class="col-xs-8">
-  		<div class="form-group">
-			<label for="content"><?= lang('comment_form_field_content') ?></label>
-			<p class="help-block"><?= lang('comment_form_field_content_hlp_txt') ?></p>
-			<?= form_textarea(['name' => 'content', 'class' => 'form-control', 'value' => $comment['content'], 'placeholder' => lang('comment_form_field_content')]) ?>
-  		</div>
-
-  		<div class="form-group">
-			<input class="btn btn-lg btn-default btn-block" type="submit" value="<?php echo lang('comments_save_btn');?>">
-  		</div>
-	</div>
-
-	<div class="col-xs-4">
-		<h4><?= lang('comment_details_hdr') ?></h4>
-		<p><?= lang('comments_blog_post_hdr') ?>: <?= $comment['post_title'] ?> (<?= $comment['post_id'] ?>)</p>
-		<p><?= lang('comments_comment_id') ?>: <?= $comment['id'] ?></p>
-		<p><?= lang('comments_author') ?>: <?= $comment['display_name'] ?></p>
-		<p><?= lang('comments_date_posted') ?>: <?= $comment['date'] ?></p>
-		<p><?= lang('comments_current_status') ?>: <? echo ($comment['modded'] == 1) ? "Moderated" : "Published"; ?></p>
-	</div>
-
-</div>
-</form>
-
+			<div class="panel panel-default">
+				  <div class="panel-heading">
+					<h3 class="panel-title"><?php echo lang('comment_details_hdr'); ?></h3>
+				  </div>
+				  <div class="panel-body">
+						<p><b><?php echo lang('comments_blog_post_hdr'); ?></b>: <?php echo $comment['post_title']; ?> (<?php echo $comment['post_id']; ?>)</p>
+						<p><b><?php echo lang('comments_comment_id'); ?></b>: <?php echo $comment['id']; ?></p>
+						<p><b><?php echo lang('comments_author'); ?></b>: <?php echo $comment['display_name']; ?></p>
+						<p><b><?php echo lang('comments_date_posted'); ?></b>: <?php echo $comment['date']; ?></p>
+						<p><b><?php echo lang('comments_author_ip'); ?></b>: <?php echo $comment['author_ip']; ?></p>
+						<p><b><?php echo lang('comments_current_status'); ?></b>: <?php echo ($comment['modded'] == 1) ? "Moderated" : "Published"; ?></p>
+				  </div>
+			</div>
+			</div>
+			</div>
+			</form>
+		  </div>
+			</div>	
--- application/views/admin/comments/index.php
+++ application/views/admin/comments/index.php
@@ -1,125 +1,176 @@
-<h2><?= lang('comments_hdr') ?></h2>
-<div>
-
-    <!-- Nav tabs -->
+<?php 
+ /**
+ * Manage Comments
+ * 
+ * @access       public
+ * @author       Enliven Appications
+ * @version      3.0
+ * Last Updated  May 04, 2017
+ * used bootstrap panels, breadcrumbs and alerts; added JQuery Datatables; Updated validation and also added some language variables 
+ * @author       Simon Montaño
+*/
+?>
+        <div class="btn-group btn-breadcrumb">
+            <a href="<?php echo site_url('admin'); ?>" class="btn btn-default"><i class="glyphicon glyphicon-home"></i></a>
+            <a href="<?php echo site_url('admin_comments'); ?>" class="btn btn-default"><?php echo lang('comments_section_name'); ?></a> 
+			<a href="#" class="btn btn-primary"><?php echo lang('comments_index_page'); ?></a>
+         </div>		 
+		 <br /> &nbsp; <br /> 
+ 
+		 <div class="panel panel-default">
+		  <div class="panel-heading">
+			<h3 class="panel-title"><?php echo lang('comments_hdr'); ?> </h3>
+		  </div>
+		  <div class="panel-body">
+		  
+  <!-- Nav tabs -->
     <ul class="nav nav-tabs" role="tablist">
-        <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab"><?= lang('comments_tab_moderated') ?></a></li>
-        <li role="presentation"><a href="#active-comments" aria-controls="active-comments" role="tab" data-toggle="tab"><?= lang('comments_tab_active') ?></a></li>
+        <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab"><?php echo lang('comments_tab_moderated'); ?></a></li>
+        <li role="presentation"><a href="#active-comments" aria-controls="active-comments" role="tab" data-toggle="tab"><?php echo lang('comments_tab_active'); ?></a></li>
     </ul>
-
+    <br /> &nbsp; <br /> 
     <!-- Tab panes -->
     <div class="tab-content">
         <div role="tabpanel" class="tab-pane fade in active" id="home">
-            <?php if ($modded_comments): ?>
-            <table class="table table-condensed table-responsive">
+				
+            <?php if (empty($modded_comments)){ ?>
+ 			 <br /> &nbsp; <br />
+			<div class="alert alert-info alert-dismissable">
+			  <button type="button" class="close" data-dismiss="alert">&times;</button>
+			  <strong><?php echo lang('error_title_important'); ?></strong> <br /> &nbsp; <br />  <h4><?php echo lang('comments_no_comments_found_mod'); ?></h4>
+			</div>          
+            <?php } else { ?>
+             <table id="listing" class="table table-condensed table-responsive">
+			 <thead>		
                 <tr>
-                    <th><?= lang('comments_tbl_hdr_post_title') ?></th>
-                    <th><?= lang('comments_tbl_hdr_author') ?></th>
-                    <th><?= lang('comments_tbl_hdr_date') ?></th>
-                    <th><?= lang('comments_tbl_hdr_short_excerpt') ?></th>
+                    <th><?php echo lang('comments_tbl_hdr_post_title'); ?></th>
+                    <th><?php echo lang('comments_tbl_hdr_author'); ?></th>
+                    <th><?php echo lang('comments_tbl_hdr_date'); ?></th>
                     <th></th>
                 </tr>   
+                </thead>
+                <tbody>				
                 <?php foreach ($modded_comments as $com): ?>
                 <tr>
-                    <td><?= $com['post_title'] ?></td>
-                    <td><?= $com['display_name'] ?></td>
-                    <td><?= $com['date'] ?></td>
-                    <td><?php echo word_limiter($com['content'], 10, '&#8230;') ?></td>
+                    <td><?php echo $com['post_title']; ?></td>
+                    <td><?php echo $com['display_name']; ?></td>
+                    <td><?php echo $com['date']; ?></td>
                     <td class="text-right">
-                        <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#moddedModal<?= $com['id'] ?>"><?= lang('comments_btn_view') ?></button>
-                        <a href="<?= site_url('admin_comments/accept_comment/' . $com['id']) ?>" class="btn btn-xs btn-default"><?= lang('comments_btn_accept') ?></a>
-                        <a href="<?= site_url('admin_comments/edit_comment/' . $com['id']) ?>" class="btn btn-xs btn-default"><?= lang('comments_btn_edit') ?></a>
-                        <a href="<?= site_url('admin_comments/remove_comment/' . $com['id']) ?>" class="btn btn-xs btn-danger"><?= lang('comments_btn_remove') ?></a>
+                        <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#moddedModal<?php echo $com['id']; ?>"><?php echo lang('comments_btn_view'); ?></button>
+                        <a href="<?php echo site_url('admin_comments/accept_comment/' . $com['id']); ?>" class="btn btn-xs btn-default"><?php echo lang('comments_btn_accept'); ?></a>
+                        <a href="<?php echo site_url('admin_comments/edit_comment/' . $com['id']); ?>" class="btn btn-xs btn-default"><?php echo lang('comments_btn_edit'); ?></a>
+                        <a href="<?php echo site_url('admin_comments/remove_comment/' . $com['id']); ?>" class="btn btn-xs btn-danger"><?php echo lang('comments_btn_remove'); ?></a>
                     </td>
                 </tr>
                 <?php endforeach ?>
+				  </tbody>		
+				  <tfoot>
+				  <tr>
+                    <th><?php echo lang('comments_tbl_hdr_post_title'); ?></th>
+                    <th><?php echo lang('comments_tbl_hdr_author'); ?></th>
+                    <th><?php echo lang('comments_tbl_hdr_date'); ?></th>
+                    <th></th>				  
+	              </tr>		
+		          </tfoot>   			  
             </table>
-            <?php else: ?>
-            <h4 class="text-center m-t-l"><?= lang('comments_no_comments_found_mod') ?></h4>
-            <?php endif ?>
+
+  		<?php } ?>
       
         </div>
 
         <div role="tabpanel" class="tab-pane fade" id="active-comments">
-            <?php if ($active_comments): ?>
-            <table class="table table-condensed table-responsive">
-                <tr>
-                    <th><?= lang('comments_tbl_hdr_post_title') ?></th>
-                    <th><?= lang('comments_tbl_hdr_author') ?></th>
-                    <th><?= lang('comments_tbl_hdr_date') ?></th>
-                    <th><?= lang('comments_tbl_hdr_short_excerpt') ?></th>
+		 
+            <?php if (empty($active_comments)){ ?>
+			
+			    <br /> &nbsp; <br />
+				<div class="alert alert-info alert-dismissable">
+				  <button type="button" class="close" data-dismiss="alert">&times;</button>
+				  <strong><?php echo lang('error_title_important'); ?></strong> <br /> &nbsp; <br /> <h4><?php echo lang('comments_no_comments_found_act'); ?></h4>
+				</div>			
+			
+			<?php } else { ?>
+              <table class="display table table-condensed table-responsive">
+              <thead>
+   	    		<tr>
+                    <th><?php echo lang('comments_tbl_hdr_post_title'); ?></th>
+                    <th><?php echo lang('comments_tbl_hdr_author'); ?></th>
+                    <th><?php echo lang('comments_tbl_hdr_date'); ?></th>
                     <th></th>
                 </tr>   
+                </thead>
+                <tbody>					
                 <?php foreach ($active_comments as $com): ?>
                 <tr>
-                    <td><?= $com['post_title'] ?></td>
-                    <td><?= $com['display_name'] ?></td>
-                    <td><?= $com['date'] ?></td>
-                    <td><?php echo word_limiter($com['content'], 10, '&#8230;') ?></td>
+                    <td><?php echo $com['post_title']; ?></td>
+                    <td><?php echo $com['display_name']; ?></td>
+                    <td><?php echo $com['date']; ?></td>
                     <td class="text-right">
-                        <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#activeModal<?= $com['id'] ?>"><?= lang('comments_btn_view') ?></button>
-                        <a href="<?= site_url('admin_comments/hide_comment/' . $com['id']) ?>" class="btn btn-xs btn-warning"><?= lang('comments_btn_hide') ?></a>
-                        <a href="<?= site_url('admin_comments/edit_comment/' . $com['id']) ?>" class="btn btn-xs btn-default"><?= lang('comments_btn_edit') ?></a>
-                        <a href="<?= site_url('admin_comments/remove_comment/' . $com['id']) ?>" class="btn btn-xs btn-danger"><?= lang('comments_btn_remove') ?></a>
+                        <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#activeModal<?php echo $com['id'] ?>"><?php echo lang('comments_btn_view') ?></button>
+                        <a href="<?php echo site_url('admin_comments/hide_comment/' . $com['id']); ?>" class="btn btn-xs btn-warning"><?php echo lang('comments_btn_hide'); ?></a>
+                        <a href="<?php echo site_url('admin_comments/edit_comment/' . $com['id']); ?>" class="btn btn-xs btn-default"><?php echo lang('comments_btn_edit'); ?></a>
+                        <a href="<?php echo site_url('admin_comments/remove_comment/' . $com['id']); ?>" class="btn btn-xs btn-danger"><?php echo lang('comments_btn_remove'); ?></a>
                     </td>
                 </tr>
                 <?php endforeach ?>
-            </table>
-            <?php else: ?>
-            <h4 class="text-center m-t-l"><?= lang('comments_no_comments_found_act') ?></h4>
-            <?php endif ?>
+				  </tbody>		
+				  <tfoot>
+   	    		   <tr>
+                    <th><?php echo lang('comments_tbl_hdr_post_title'); ?></th>
+                    <th><?php echo lang('comments_tbl_hdr_author'); ?></th>
+                    <th><?php echo lang('comments_tbl_hdr_date'); ?></th>
+                    <th></th>
+                   </tr>   	
+		          </tfoot>  				
+            </table>			
+			<?php } ?>		 
 
-        </div>
-    </div>
-</div>
-
-
-<!-- modals -->
+           </div>
+         </div>		 
+	   </div>
+	</div>
+ <!-- modals -->
 <?php foreach ($active_comments as $com): ?>
     <!-- Modal -->
-<div class="modal fade" id="activeModal<?= $com['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="activeModal<?php echo $com['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"><?= $com['display_name'] ?> : <?= $com['date'] ?></h4>
+        <h4 class="modal-title" id="myModalLabel"><?php echo $com['display_name']; ?> : <?php echo $com['date']; ?></h4>
       </div>
       <div class="modal-body">
-        <?= $com['content'] ?>
+        <?php echo $com['content']; ?>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <a href="<?= site_url('admin_comments/hide_comment/' . $com['id']) ?>" class="btn btn-warning"><?= lang('comments_btn_hide') ?></a>
-        <a href="<?= site_url('admin_comments/edit_comment/' . $com['id']) ?>" class="btn btn-default"><?= lang('comments_btn_edit') ?></a>
-        <a href="<?= site_url('admin_comments/remove_comment/' . $com['id']) ?>" class="btn btn-danger"><?= lang('comments_btn_remove') ?></a>
+        <button type="button" class="btn btn-default" data-dismiss="modal"><?php echo lang('button_close'); ?></button>
+        <a href="<?php echo site_url('admin_comments/hide_comment/' . $com['id']); ?>" class="btn btn-warning"><?php echo lang('comments_btn_hide'); ?></a>
+        <a href="<?php echo site_url('admin_comments/edit_comment/' . $com['id']); ?>" class="btn btn-default"><?php echo lang('comments_btn_edit'); ?></a>
+        <a href="<?php echo site_url('admin_comments/remove_comment/' . $com['id']); ?>" class="btn btn-danger"><?php echo lang('comments_btn_remove'); ?></a>
       </div>
     </div>
   </div>
 </div>
 <?php endforeach ?>
-
 
 <?php foreach ($modded_comments as $com): ?>
-    <!-- Modal -->
-<div class="modal fade" id="moddedModal<?= $com['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<!-- Modal -->
+<div class="modal fade" id="moddedModal<?php echo $com['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"><?= $com['display_name'] ?> : <?= $com['date'] ?></h4>
+        <h4 class="modal-title" id="myModalLabel"><?php echo $com['display_name']; ?> : <?php echo $com['date']; ?></h4>
       </div>
       <div class="modal-body">
-        <?= $com['content'] ?>
+        <?php echo $com['content']; ?>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <a href="<?= site_url('admin_comments/accept_comment/' . $com['id']) ?>" class="btn btn-success"><?= lang('comments_btn_accept') ?></a>
-        <a href="<?= site_url('admin_comments/edit_comment/' . $com['id']) ?>" class="btn btn-default"><?= lang('comments_btn_edit') ?></a>
-        <a href="<?= site_url('admin_comments/remove_comment/' . $com['id']) ?>" class="btn btn-danger"><?= lang('comments_btn_remove') ?></a>
+        <button type="button" class="btn btn-default" data-dismiss="modal"><?php echo lang('button_close'); ?></button>
+        <a href="<?php echo site_url('admin_comments/accept_comment/' . $com['id']); ?>" class="btn btn-success"><?php echo lang('comments_btn_accept'); ?></a>
+        <a href="<?php echo site_url('admin_comments/edit_comment/' . $com['id']); ?>" class="btn btn-default"><?php echo lang('comments_btn_edit'); ?></a>
+        <a href="<?php echo site_url('admin_comments/remove_comment/' . $com['id']); ?>" class="btn btn-danger"><?php echo lang('comments_btn_remove'); ?></a>
       </div>
     </div>
   </div>
 </div>
 <?php endforeach ?>
-
-
