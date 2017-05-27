fcda001206146933f5c4ebac600eca71d28c67f6
Simon <simon_montano@yahoo.com>
2017-05-27 16:40:29 -0400

    Add files via upload

--- application/themes/default/layouts/default.php
+++ application/themes/default/layouts/default.php
@@ -1,28 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+//error_reporting(0);
+?>
 <!DOCTYPE html>
-<html lang="<?= $this->session->language_abbr ?>">
+<html lang="<?php echo $this->session->language_abbr; ?>">
 	<head>
-		<meta charset="utf-8">
-    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-    	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-		<title><?php echo $template['title']; ?></title>
+	<!--[if IE]>
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+	<![endif]-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> <?php // echo $page['title']; ?> | <?php echo $template['title']; ?></title>
     <!-- if you create CDN links, do that first before echoing $template['metadata'] so you can override default CDN settings -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		
-    <!-- add css and js before echoing $template['metadata'] -->
-    <?php $this->template->append_css('default.css') ?>
-
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/font-awesome.min.css"); ?>">
+    <!-- add css and js before echoing $template['metadata'] -->    
+	<?php $this->template->append_css('default.css'); ?>
+	
     <!-- echo css, js, and other metadata as defined -->
     <?php echo $template['metadata']; ?>
+	
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script type="text/javascript" src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script type="text/javascript" src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->	
+ <link rel="shortcut icon" href="<?php echo base_url(); ?>/assets/favicon.ico" />	
 	</head>
 	<body>
 		<!-- temp -->
 
-
-
 <div class="wrapper">
 
-    <div class="box" style="background-image:url('<?php echo Asset::get_filepath_img('bg_suburb.jpg'); ?>');">
+    <div class="box">
         <div class="row">
 
           <?php if ( $this->ion_auth->logged_in() ): ?>
@@ -58,8 +68,9 @@
                   <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">User <span class="caret"></span></a>
                     <ul class="dropdown-menu">
+					  <li><a href="<?php echo site_url('auth/change_password') ?>"><b>Change Password</b></a></li>
+                      <li><a href="<?php echo site_url('auth/edit_user/' . $this->ion_auth->get_user_id()) ?>"><b>Edit Profile</b></a></li>					
                       <li><a href="<?php echo site_url('auth/logout') ?>"><b>Log Out</b></a></li>
-                      <li><a href="<?php echo site_url('auth/edit_user/' . $this->ion_auth->get_user_id()) ?>"><b>Edit Profile</b></a></li>
                     </ul>
                   </li>
 
@@ -80,13 +91,10 @@
           
             <!-- main -->
             <div class="column col-sm-9" id="main">
-                <div class="padding">
-                    <div class="full col-sm-9">
-                      <div class="row">
-                        <div class="col-sm-10">
-                            <h2><?php echo $template['title']; ?></h2>
-                        </div>
-                        <?php if ($this->template->lang_picker): ?>
+			<div class="padding">
+			 <div class="full col-sm-9">
+			<div class="text-right">
+			  <?php if ($this->template->lang_picker): ?>
                           <div class="col-sn-2">
                             <div class="btn-group">
                               <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -94,48 +102,28 @@
                               </button>
                               <ul class="dropdown-menu">
                                 <?php foreach ($this->template->lang_picker as $lang): ?>
-                                <li><?= $lang ?></li>
+                                <li><?php echo $lang; ?></li>
                                 <?php endforeach ?>
                                 
                               </ul>
                             </div>
                           </div>
                         <?php endif ?>
+			 </div>
+			
+			
+                
+                   
+                      <div class="row">
+                        <div class="col-sm-12">
+                            <h2 class="page-header"><?php echo $template['title']; ?></h2>
+                        </div>
+                      
                       </div>
-                      
-
-                      
+                        
                         <!-- content -->
+                        <br /> &nbsp; <br />
 
-                        <?php if ($this->session->flashdata()): ?>
-                        <div class="flashData">
-                              <?php if($this->session->flashdata('success')): ?>
-                              <div class="alert alert-success" role="alert">
-                                <?php echo $this->session->flashdata('success') ?>
-                              </div>
-                              <?php endif ?>
-
-                              <?php if($this->session->flashdata('error')): ?>
-                              <div class="alert alert-danger" role="alert">
-                                <?php echo $this->session->flashdata('error') ?>
-                              </div>
-                              <?php endif ?>
-
-                              <?php if($this->session->flashdata('info')): ?>
-                              <div class="alert alert-info" role="alert">
-                                <?php echo $this->session->flashdata('info') ?>
-                              </div>
-                              <?php endif ?>
-
-                              <?php if($this->session->flashdata('warning')): ?>
-                              <div class="alert alert-warning" role="alert">
-                                <?php echo $this->session->flashdata('warning') ?>
-                              </div>
-                              <?php endif ?>
-
-
-                            </div>
-                            <?php endif ?>
       
                         <?php echo $template['body']; ?>
 
@@ -176,12 +164,12 @@
                         <div class="row" id="footer">    
                           <div class="col-sm-6">
                             <p class="">
-                            <a href="http://www.bootply.com">Theme: Bootply</a> | <a href="http://open-blog.org">Powered by Open Blog</a>
+                            Theme: <a href="http://www.bootply.com" target="_blank">Bootply</a> | Powered by <a href="http://open-blog.org"  target="_blank">Open Blog</a>
                             </p>
                           </div>
                           <div class="col-sm-6 text-right">
                             <p>
-                              <a href="#">© Copyright <?php echo date('Y') ?> <?php echo $template['title']; ?></a>
+                             &copy; Copyright <?php echo date('Y'); ?> | <a href="#"><?php echo $template['title']; ?></a>
                             </p>
                           </div>
                         </div>
@@ -196,10 +184,10 @@
 
 <!-- temp -->
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+     <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-3.2.1.min.js"); ?>"></script> 
+     <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
+     <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.easing.1.3.js"); ?>"></script>
+     <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.ui.totop.js"); ?>"></script>
+     <script type="text/javascript" src="<?php echo base_url("assets/js/2top.js"); ?>"></script>		
 	</body>
 </html>
-
-
-
