7412546836266039b53d91214f9fb8c85210edad
Simon <simon_montano@yahoo.com>
2017-05-27 16:29:22 -0400

    Add files via upload

--- application/views/admin/links/add_link.php
+++ application/views/admin/links/add_link.php
@@ -1,44 +1,114 @@
-<div class="container">
+<?php 
+ /**
+ * Add New Link
+ * 
+ * @access       public
+ * @author       Enliven Appications
+ * @version      3.0
+ * Last Updated  May 04, 2017
+ * used bootstrap panels, breadcrumbs and alerts; Changed way to show validation messages and also added some language variables 
+ * @author       Simon Montaño
+*/
+?>
+ 
+         <div class="btn-group btn-breadcrumb">
+            <a href="<?php echo site_url('admin'); ?>" class="btn btn-default"><i class="glyphicon glyphicon-home"></i></a>
+            <a href="<?php echo site_url('admin_links'); ?>" class="btn btn-default"><?php echo lang('links_hdr'); ?></a>
+			<a href="#" class="btn btn-primary"><?php echo lang('index_add_new_link'); ?></a>
+         </div>		 
+		 <br /> &nbsp; <br /> 
+ 
     <div class="row">
-		<div class="col-md-4 col-md-offset-4">
-			<?= validation_errors() ?>
+	
+	<?php 
+		   if (validation_errors()) {  ?>
+			<div class="alert alert-danger alert-dismissable">
+			  <button type="button" class="close" data-dismiss="alert">&times;</button>
+			  <strong><?php echo lang('error_title_important'); ?></strong> <br /> 
+			  <?php echo lang('error_general_message'); ?>
+			</div>
+         <?php } ?>
 
 			<?php if (isset($message)): ?>
 				<div class="alert alert-danger" role="alert">
-					<?= $message ?>
+					<?php echo $message; ?>
 				</div>
 			<?php endif ?>
+			<?php echo form_open(current_url()); ?>
     		<div class="panel panel-default">
 			  	<div class="panel-heading">
-			    	<h3 class="panel-title"><?= lang('index_add_new_link');?></h3>
+			    	<h3 class="panel-title"><?php echo lang('index_add_new_link'); ?></h3>
 			 	</div>
 			  	<div class="panel-body">
-			    	<?= form_open(current_url());?>
-			    		<p><?= lang('add_link_subheading');?></p>
+			       		<div class="well">
+						     <?php echo lang('add_link_subheading'); ?>
+						</div>
+
                     <fieldset>
-			    	  	<div class="form-group">
-			    		    <?= form_input(['name'=> 'name', 'id' => 'name', 'class' => "form-control", 'placeholder' => lang('link_form_name'), 'value' => set_value('name')]) ?>
+			    	  	<div class="form-group<?php if (form_error('name')) { ?> has-error has-feedback <?php } ?>">
+						  <label for="name" class="col-lg-2 control-label"><?php echo $this->lang->line('link_form_name'); ?></label>
+					      <div class="col-lg-10">
+			    		    <?php echo form_input(['name'=> 'name', 'id' => 'name', 'class' => "form-control", 'placeholder' => lang('link_form_name'), 'value' => set_value('name')]); ?>
+                            <?php if (form_error('name')) { echo form_error('name'); } else { ?>
+								<p class="help-block"><?php echo $this->lang->line('link_form_name_help'); ?></p>
+							<?php } ?>				    		
+						</div>
+						</div>
+						
+			    		<div class="form-group<?php if (form_error('url')) { ?> has-error has-feedback <?php } ?>">
+	                        <label for="url" class="col-lg-2 control-label"><?php echo $this->lang->line('link_form_urlname'); ?></label>
+					      <div class="col-lg-10">		    			
+							<?php echo form_input(['name'=> 'url', 'id' => 'url', 'class' => "form-control", 'placeholder' => lang('link_form_url'), 'value' => set_value('url')]); ?>
+                           <?php if (form_error('url')) { echo form_error('url'); } else { ?>
+								<p class="help-block"><?php echo $this->lang->line('link_form_url_help'); ?></p>
+							<?php } ?>				    		
+						</div>							
+			    		</div>						
+						
+			    		<div class="form-group<?php if (form_error('target')) { ?> has-error has-feedback <?php } ?>">
+			    			 <label for="target" class="col-lg-2 control-label"><?php echo $this->lang->line('link_form_url_target'); ?></label>
+							  <div class="col-lg-10">
+							<?php echo form_dropdown('target', ['_blank' => lang('blank_window'), '_self' => lang('same_window')], ($this->input->post('target'))?$this->input->post('target'):null, 'id="target" class="form-control"'); ?>
+                            <?php if (form_error('target')) { echo form_error('target'); } else { ?>
+								<p class="help-block"><?php echo $this->lang->line('link_form_target_help'); ?></p>
+							<?php } ?>				    		
+						</div>								
 			    		</div>
-			    		<div class="form-group">
-			    			<?= form_input(['name'=> 'url', 'id' => 'url', 'class' => "form-control", 'placeholder' => lang('link_form_url'), 'value' => set_value('url')]) ?>
+						
+			    		<div class="form-group<?php if (form_error('description')) { ?> has-error has-feedback <?php } ?>">
+			    			 <label for="description" class="col-lg-2 control-label"><?php echo $this->lang->line('link_form_desc'); ?></label>
+							  <div class="col-lg-10">
+							 <?php echo form_input(['name'=> 'description', 'id' => 'description', 'class' => "form-control", 'placeholder' => lang('link_form_desc'), 'value' => set_value('description')]); ?>
+		                     <?php if (form_error('description')) { echo form_error('description'); } else { ?>
+								<p class="help-block"><?php echo $this->lang->line('link_form_description_help'); ?></p>
+							<?php } ?>						 
 			    		</div>
-			    		<div class="form-group">
-			    			<?= form_dropdown('target', ['_blank' => lang('blank_window'), '_self' => lang('same_window')], ($this->input->post('target'))?$this->input->post('target'):null, 'class="form-control"') ?>
+						</div>	
+						
+			    		<div class="form-group<?php if (form_error('visible')) { ?> has-error has-feedback <?php } ?>">
+						    <label for="visible" class="col-lg-2 control-label"><?php echo $this->lang->line('link_form_visibility'); ?></label>
+							 <div class="col-lg-10">
+			    			<?php echo form_dropdown('visible', ['1' => lang('visible'), '0' => lang('not_visible')], ($this->input->post('visible'))?$this->input->post('visible'):null, ' id="visible" class="form-control"'); ?>
+                            <?php if (form_error('visible')) { echo form_error('visible'); } else { ?>
+								<p class="help-block"><?php echo $this->lang->line('link_form_visibility_help'); ?></p>
+							<?php } ?>								
 			    		</div>
-			    		<div class="form-group">
-			    			<?= form_input(['name'=> 'description', 'id' => 'description', 'class' => "form-control", 'placeholder' => lang('link_form_desc'), 'value' => set_value('description')]) ?>
+						</div>
+						
+			    		<div class="form-group<?php if (form_error('position')) { ?> has-error has-feedback <?php } ?>">
+			    			 <label for="position" class="col-lg-2 control-label"><?php echo $this->lang->line('link_form_position'); ?></label>
+							 <div class="col-lg-10">
+							 <?php echo form_input(['name'=> 'position', 'id' => 'position', 'class' => "form-control", 'placeholder' => lang('link_form_position'), 'value' => set_value('position')]); ?>
+							  <?php if (form_error('position')) { echo form_error('position'); } else { ?>
+								<p class="help-block"><?php echo $this->lang->line('link_form_position_help'); ?></p>
+							<?php } ?>	
 			    		</div>
-			    		<div class="form-group">
-			    			<?= form_dropdown('visible', ['1' => lang('visible'), '0' => lang('not_visible')], ($this->input->post('visible'))?$this->input->post('visible'):null, 'class="form-control"') ?>
-			    		</div>
-			    		<div class="form-group">
-			    			<?= form_input(['name'=> 'position', 'id' => 'position', 'class' => "form-control", 'placeholder' => lang('link_form_position'), 'value' => set_value('position')]) ?>
-			    		</div>
-			    		<input class="btn btn-lg btn-default btn-block" type="submit" value="<?php echo lang('save_link_btn');?>">
+						</div>
+						<br /> &nbsp; <br />	
+			    		<input class="btn btn-lg btn-default btn-block" type="submit" value="<?php echo lang('save_link_btn'); ?>">
 			    	</fieldset>
-			      	</form>
+			      
 			    </div>
 			</div>
-		</div>
-	</div>
+	        </form>	 
 </div>
--- application/views/admin/links/index.php
+++ application/views/admin/links/index.php
@@ -1,29 +1,94 @@
-<h2><?= lang('links_hdr') ?></h2>
+<?php 
+ /**
+ * Links Main Page
+ * 
+ * @access       public
+ * @author       Enliven Appications
+ * @version      3.0
+ * Last Updated  May 04, 2017
+ * used bootstrap panels, breadcrumbs and alerts; Changed way to show validation messages, also added some language variables and deleted a javascript code used here to reorder links
+ * @author       Simon Montaño
+*/
+?>  
 
-<p><a class="btn btn-default btn-sm" href="<?php echo site_url('admin_links/add_link') ?>"><?php echo lang('index_add_new_link') ?></a></p>
+        <div class="btn-group btn-breadcrumb">
+            <a href="<?php echo site_url('admin'); ?>" class="btn btn-default"><i class="glyphicon glyphicon-home"></i></a>
+            <a href="<?php echo site_url('admin_links'); ?>" class="btn btn-default"><?php echo lang('links_hdr'); ?></a>
+			<a href="#" class="btn btn-primary"><?php echo lang('links_index_page'); ?></a>
+         </div>		  
+		 <br /> &nbsp; <br /> 
 
-<table class="table table-condensed">
-    <tr>
-        <th>Name</th>
-        <th>URL</th>
-        <th>target</th>
-        <th>Description</th>
-        <th>Visible</th>
-        <th>Order</th>
-        <th></th>
-    </tr>
-    <?php foreach ($links as $link): ?>
+<div class="text-right"><a class="btn btn-default btn-sm" href="<?php echo site_url('admin_links/add_link') ?>"><?php echo lang('index_add_new_link') ?> <i class="fa fa-plus" aria-hidden="true"></i></a></div>
+ <br />
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title"><?php echo lang('links_hdr'); ?></h3>
+  </div>
+  <div class="panel-body">
+ 
+ <?php
+	 
+	if (empty($links)) { ?>   
+
+	<div class="alert alert-warning alert-dismissable">
+	  <button type="button" class="close" data-dismiss="alert">&times;</button>
+	  <strong><?php echo lang('error_title_important'); ?></strong> 
+	  <br />
+	  <?php echo lang('error_no_links'); ?>
+	</div>
+	  
+	<?php } else  { ?> 
+    
+  <table id="listing" class="table table-striped table-bordered">
+        <thead>
+            <tr>
+				<th><?php echo lang('title_name'); ?></th>
+				<th><?php echo lang('social_form_url'); ?></th>
+				<th><?php echo lang('title_url_target'); ?></th>
+				<th><?php echo lang('title_description'); ?></th>
+				<th><?php echo lang('title_visible'); ?></th>
+				<th><?php echo lang('title_order'); ?></th>
+				<th><?php echo lang('title_edit'); ?></th>
+				<th><?php echo lang('title_delete'); ?></th>				
+		    </tr>
+		 </thead>
+        <tbody>	
+		<?php foreach ($links as $link): ?>
         <tr>
-        <td><?= $link->name ?></td>
-        <td><a href="<?= $link->url ?>" target="_blank"><?= $link->url ?></a></td>
-        <td><?= $link->target ?></td>
-        <td><?= $link->description ?></td>
-        <td><?= $link->visible ?></td>
-        <td><?= $link->position ?></td>
+        <td><?php echo $link->name; ?></td>
+        <td><a href="<?php echo $link->url; ?>" target="_blank"><?php echo $link->url; ?></a></td>
+        <td><?php echo $link->target; ?></td>
+        <td><?php echo $link->description; ?></td>
+        <td><?php echo $link->visible; ?></td>
+        <td><?php echo $link->position; ?></td>
         <td>
-            <a href="<?= site_url('admin_links/edit_link/' . $link->id) ?>" class="btn btn-default btn-xs"><?= lang('link_edit_btn') ?></a>
-            <a href="<?= site_url('admin_links/remove_link/' . $link->id) ?>" class="btn btn-danger btn-xs"><?= lang('link_remove_btn') ?></a>
+            <a href="<?php echo site_url('admin_links/edit_link/' . $link->id); ?>" class="btn btn-default btn-xs">
+			<i class="fa fa-pencil" aria-hidden="true"></i> <?php echo lang('link_edit_btn'); ?></a>
+		</td>	
+        <td>
+		    <a href="<?php echo site_url('admin_links/remove_link/' . $link->id); ?>" class="btn btn-danger btn-xs">
+			<i class="fa fa-trash" aria-hidden="true"></i> <?php echo lang('link_remove_btn'); ?></a>
         </td>
     </tr>
   <?php endforeach ?>
-</table>
+  </tbody>		
+        <tfoot>
+            <tr>
+				<th><?php echo lang('title_name'); ?></th>
+				<th><?php echo lang('social_form_url'); ?></th>
+				<th><?php echo lang('title_url_target'); ?></th>
+				<th><?php echo lang('title_description'); ?></th>
+				<th><?php echo lang('title_visible'); ?></th>
+				<th><?php echo lang('title_order'); ?></th>
+				<th><?php echo lang('title_edit'); ?></th>
+				<th><?php echo lang('title_delete'); ?></th>				
+            </tr>
+        </tfoot>
+
+    </table> 
+	
+	<?php } ?>
+ 
+  </div>
+</div>
