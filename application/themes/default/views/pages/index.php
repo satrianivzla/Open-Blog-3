eadc0d4551f9f49b4da7cf70ceb5941a7524ac78
Simon <simon_montano@yahoo.com>
2017-05-27 16:36:01 -0400

    Add files via upload

--- application/themes/default/views/pages/index.php
+++ application/themes/default/views/pages/index.php
@@ -1,22 +1,29 @@
-<div class="col-sm-12" id="recent">   
-  <div class="page-header text-muted">
-  	<?= $page['title'] ?>
-  </div> 
-</div>
 
 
 	<div class="row">    
-      <div class="col-sm-12">
-        <p><?php echo $page['content'] ?></p>
-        <h4>
-          <small class="text-muted">
+      <div class="col-sm-12" id="recent">
+	  
+	  <div class="panel panel-default">
+		  <div class="panel-heading">
+			<h3 class="panel-title"><?php echo $page['title']; ?></h3>
+		  </div>
+		  <div class="panel-body">
+			<?php echo $page['content']; ?> 
+		  </div>
+		</div>
+			  
+	  
+         
+       
             <?php if ( $this->ion_auth->is_admin() || $this->ion_auth->in_group('editor') || $this->ion_auth->logged_in() && $this->session->userdata('user_id') == $post->author  ): ?>
-            <a class="btn btn-default text-muted" href="<?php echo site_url('admin_pages/edit_page/' . $page['id']) ?>"><?php echo lang('btn_edit'); ?></a> 
+			 <h4>
+                <small class="text-muted">
+                <a class="btn btn-default text-muted" href="<?php echo site_url('admin_pages/edit_page/' . $page['id']); ?>"><?php echo lang('btn_edit'); ?></a> 
+				 <br />         
+                </small>
+            </h4>
             <?php endif ?>
-            <br>
-            
-        </small>
-      </h4>
+           
       </div>
 
     </div>
