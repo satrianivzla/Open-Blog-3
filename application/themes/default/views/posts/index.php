84fed834d4cd28e1910b726bb96475b9ca104332
Simon <simon_montano@yahoo.com>
2017-05-27 16:35:26 -0400

    Add files via upload

--- application/themes/default/views/posts/index.php
+++ application/themes/default/views/posts/index.php
@@ -1,12 +1,15 @@
+
+
 <div class="col-sm-12" id="recent">   
   <div class="page-header text-muted">
     <?php if (! $this->uri->segment(1)): ?>
   <?php echo lang('recent_posts'); ?>
 <?php else: ?>
+
   <?php if ($this->uri->segment(2) == 'category'): ?>
-    <?= lang('category_hdr') . ' `' . ucfirst($this->uri->segment(3)) . '`'  ?> 
+    <?php echo lang('category_hdr') . ' `' . ucfirst($this->uri->segment(3)) . '`'  ?> 
   <?php elseif ($this->uri->segment(2) == 'archive'): ?>
-    <?= lang('archives_for_hdr') . ' ' . date('F', $this->uri->segment(4)) . ' ' . $this->uri->segment(3)  ?> 
+    <?php echo lang('archives_for_hdr') . ' ' . date('F', $this->uri->segment(4)) . ' ' . $this->uri->segment(3)  ?> 
 
   <?php else: ?>
     <?php echo lang('older_posts'); ?>
@@ -15,48 +18,66 @@
   </div> 
 </div>
 
+<div class="clearfix"> <br /> </div>
 <?php if ($posts): ?>
 <?php foreach ($posts as $post): ?>
 
+
 	<div class="row">    
-      <div class="col-sm-10">
-        <h3><?php echo $post->title ?></h3>
-        <h4>
+      <div class="col-sm-12">
+
+		<div class="panel panel-default">
+		  <div class="panel-heading">
+			<h3 class="panel-title"><a href="<?php echo $post->url; ?>"><?php echo $post->title; ?></a></h3>
+		  </div>
+		  <div class="panel-body">
+		  
+		<?php if($post->feature_image): ?>
+          <a href="<?php echo $post->url; ?>"><img src="<?php echo base_url('uploads/' .$post->feature_image); ?>" class="img-responsive" alt="<?php echo $post->title; ?>" title="<?php echo $post->title; ?>"></a>
+		  <?php else: ?>
+		   <a href="<?php echo $post->url; ?>"><img src="<?php echo base_url('assets/images/no-image-header.jpg'); ?>" class="img-responsive" alt="<?php echo $post->title; ?>" title="<?php echo $post->title; ?>"></a>
+        <?php endif ?>		  
+		  
+		  
+	      <h4>
           <small class="text-muted">
-          	<span class="glyphicon glyphicon-time" aria-hidden="true"><time class="post-date" datetime="<?php echo date("D, d M Y H:i:s T", strtotime($post->date_posted)) ?>"></span> <?= $post->date_posted ?>
-          	<span class="glyphicon glyphicon-user" aria-hidden="true"></span> <?php echo $post->display_name ?>
-          	<span class="glyphicon glyphicon-comment" aria-hidden="true"></span> <?php echo $post->comment_count ?>
+          	<span class="glyphicon glyphicon-time" aria-hidden="true"><time class="post-date" datetime="<?php echo date("D, d M Y H:i:s T", strtotime($post->date_posted)); ?>"></time></span> <?php echo $post->date_posted; ?>
+          	<span class="glyphicon glyphicon-user" aria-hidden="true"></span> <?php echo $post->display_name; ?>
+          	<span class="glyphicon glyphicon-comment" aria-hidden="true"></span> <?php echo $post->comment_count; ?>
             <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
             <?php foreach ($post->categories as $cat): ?>
-                <?php echo $cat->name ?> 
+                <?php echo $cat->name; ?> 
               <?php endforeach ?>               
           </small>
-        </h4>
-        <p><?php echo $post->excerpt ?></p>
+         </h4>
+        <p><?php echo $post->excerpt; ?></p>
+		 <br /> &nbsp;  <br />
         <h4>
           <small class="text-muted">
-            <a class="btn btn-default text-muted" href="<?php echo $post->url ?>"><?php echo lang('btn_read_more'); ?></a> 
+            <a class="btn btn-default text-muted" href="<?php echo $post->url; ?>"><?php echo lang('btn_read_more'); ?></a> 
             <?php if ( $this->ion_auth->is_admin() || $this->ion_auth->in_group('editor') || $this->ion_auth->logged_in() && $this->session->userdata('user_id') == $post->author  ): ?>
-            <a class="btn btn-default text-muted" href="<?php echo site_url('admin_posts/edit_post/' . $post->id) ?>"><?php echo lang('btn_edit'); ?></a> 
+            <a class="btn btn-default text-muted" href="<?php echo site_url('admin_posts/edit_post/' . $post->id); ?>"><?php echo lang('btn_edit'); ?></a> 
             <?php endif ?>
-            <br>
-            
+            <br />            
         </small>
       </h4>
+	  
+	  
+		  </div>
+		</div>
+
+
+
+
       </div>
 
-      <div class="col-sm-2">
-        <?php if($post->feature_image): ?>
-          <img src="<?= base_url('uploads/' . $post->feature_image) ?>" class="img-responsive" alt="Responsive image">
-        <?php endif ?>
-      </div> 
-
+ 
     </div>
 
 
 <div class="row divider">    
     <div class="col-sm-12">
-        <hr>
+        <hr />
     </div>
 </div>
 
@@ -65,13 +86,15 @@
 <?php if ($pagination): ?>
 <div class="row divider">    
     <div class="col-sm-12">
-        <?php echo $pagination ?>
+        <?php echo $pagination; ?>
     </div>
 </div>
 
 <?php endif ?>
 
 <?php else: ?>
-  <h3 class="text-center"><?= lang('no_posts_found') ?></h3>
-
+	<div class="alert alert-info alert-dismissable">
+	  <button type="button" class="close" data-dismiss="alert">&times;</button>
+	  <strong><?php echo lang('error_title_important'); ?></strong>  <h3 class="text-center"><?php echo lang('no_posts_found'); ?></h3>
+	</div>
 <?php endif ?>
--- application/themes/default/views/posts/single_post.php
+++ application/themes/default/views/posts/single_post.php
@@ -1,32 +1,44 @@
 <?php if ($post): ?>
-<div class="row">
-	<div class="col-sm-9">
-		<h3><?php echo $post['title'] ?></h3>
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title"><?php echo $post['title'] ?></h3>
+  </div>
+  <div class="panel-body">
+
+  <div class="row">
+	<div class="col-sm-12">
+		 
 		<?php if($post['feature_image']): ?>
-          <img src="<?= base_url('uploads/' . $post['feature_image']) ?>" class="img-responsive" alt="<?php echo $post['title'] ?>">
+          <img src="<?php echo base_url('uploads/' . $post['feature_image']); ?>" class="img-responsive" alt="<?php echo $post['title']; ?>" title="<?php echo $post['title']; ?>">
+		  <?php else: ?>
+		   <img src="<?php echo base_url('assets/images/no-image-header.jpg'); ?>" class="img-responsive" alt="<?php echo $post['title']; ?>" title="<?php echo $post['title']; ?>">
         <?php endif ?>
         <h4>
           <small class="text-muted">
-          	<span class="glyphicon glyphicon-time" aria-hidden="true"><time class="post-date" datetime="<?php echo date("D, d M Y H:i:s T", strtotime($post['date_posted'])) ?>"></span> <?= $post['date_posted'] ?>
-          	<span class="glyphicon glyphicon-user" aria-hidden="true"></span> <?php echo $post['display_name'] ?>
-            <span class="glyphicon glyphicon-comment" aria-hidden="true"></span> <?php echo $post['comment_count'] ?>
+          	<span class="glyphicon glyphicon-time" aria-hidden="true"><time class="post-date" datetime="<?php echo date("D, d M Y H:i:s T", strtotime($post['date_posted'])) ?>"></time></span> <?php echo $post['date_posted']; ?> |
+          	<span class="glyphicon glyphicon-user" aria-hidden="true"></span> <?php echo $post['display_name']; ?> | 
+			<i class="fa fa-eye" aria-hidden="true"></i> <?php echo $post['views']; ?> | 
+            <span class="glyphicon glyphicon-comment" aria-hidden="true"></span> <?php echo $post['comment_count']; ?> | 
             <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> 
               <?php foreach ($post['categories'] as $cat): ?>
-                <?php echo $cat->name ?> 
+                <?php echo $cat->name; ?> 
               <?php endforeach ?> 
           </small>
         </h4>
+		
+		<br />
+		
         <?php echo $post['content'] ?>
-        <h4>
-          <small class="text-muted">
-            
-        </small>
         <?php if ( $this->ion_auth->is_admin() || $this->ion_auth->in_group('editor') ): ?>
-            <br><a class="btn btn-default text-muted" href="<?php echo site_url('admin_posts/edit_post/' . $post['id']) ?>">Edit</a> 
+        <h4>
+		   <small class="text-muted"></small>
+			<br /><a class="btn btn-default text-muted" href="<?php echo site_url('admin_posts/edit_post/' . $post['id']) ?>"><?php echo lang('btn_edit'); ?></a>  
+		</h4>	
             <?php endif ?>
-      </h4>
-	<hr>
-      <h4><?php echo lang('comments_title'); ?></h4>
+      
+	<hr />
+      <h3 class="page-header"><?php echo lang('comments_title'); ?></h3>
 
       	<?php if ($post['comment_count'] > 0): ?>
 
@@ -38,12 +50,12 @@
                   <header class="text-left">
                     <div class="comment-user">
                     	<i class="glyphicon glyphicon-user"></i> 
-                    	<a href="#comment-<?php echo $comment->id; ?>"> <?php echo $comment->author; ?>
+                    	<a href="#comment-<?php echo $comment->id; ?>"> <?php echo ucwords($comment->author); ?>
                     	</a>
                     </div>
                     <time class="comment-date" datetime="<?php echo date("D, d M Y H:i:s T", strtotime($comment->date)) ?>">
                     	<i class="glyphicon glyphicon-time"></i> 
-                    	<?= $comment->date ?>
+                    	<?php echo $comment->date; ?>
                     </time>
                   </header>
                   <div class="comment-post">
@@ -57,53 +69,76 @@
           </article>
           <?php endforeach; ?>
 		<?php else: ?>
-			<p><?php echo lang('no_comments'); ?></p>
+		
+				<div class="alert alert-info alert-dismissable">
+				  <button type="button" class="close" data-dismiss="alert">&times;</button>
+				  <strong><?php echo lang('error_title_important'); ?></strong>  <br /><?php echo lang('no_comments'); ?>
+				</div>
+
+
+			  
+			
 		<?php endif; ?>
 
-	</div>
-
-
-	<div class="col-sm-3">
-
-		<h2><?php echo lang('leave_reply'); ?></h2>
+		<h2 class="page-header"><?php echo lang('leave_reply'); ?></h2>
+		
 		<?php if ($this->config->item('allow_comments') == '1' && $post['allow_comments'] != '0'): ?>
 			
-		<?php if (validation_errors()): ?>
-		<div class="error">
-			<?php echo validation_errors(); ?>
-		</div>
-		<?php endif; ?>
+         <?php 
+		   if (validation_errors()) {  ?>
+			<div class="alert alert-danger alert-dismissable">
+			  <button type="button" class="close" data-dismiss="alert">&times;</button>
+			  <strong><?php echo lang('error_title_important'); ?></strong> <br /> 
+			   <?php echo lang('error_general_message'); ?>
+ 			</div>
+         <?php } ?>
 			
-	 	<form class="form-horizontal" method="post" action="">
+	 	<form class="form-horizontal" method="post" action="#">
 
 	 		<?php if ( ! $this->ion_auth->logged_in() ): ?>
-		  <div class="form-group">
-		      <input name="nickname" id="nickname" type="text" value="<?php echo set_value('nickname'); ?>" class="form-control" placeholder="<?php echo lang('nickname'); ?>" required />
+		  <div class="form-group<?php if (form_error('nickname')) { ?> has-error has-feedback <?php } ?>">
+			<label for="nickname" class="col-lg-2 control-label"><?php echo $this->lang->line('comments_nicname'); ?></label>
+			<div class="col-lg-10">
+		      <input name="nickname" id="nickname" type="text" value="<?php echo set_value('nickname'); ?>" class="form-control" placeholder="<?php echo lang('nickname'); ?>"  />
+               <?php if (form_error('nickname')) { echo form_error('nickname'); } else { ?>
+					<p class="help-block"><?php echo $this->lang->line('cat_form_name_help'); ?></p>
+			    <?php } ?>							
+			 </div>			  
 		  </div>
-		  <div class="form-group">
-		      <input name="email" id="email" type="email" value="<?php echo set_value('email'); ?>" class="form-control" placeholder="<?php echo lang('email'); ?>" required />
+		  
+		  <div class="form-group<?php if (form_error('email')) { ?> has-error has-feedback <?php } ?>">
+			<label for="email" class="col-lg-2 control-label"><?php echo $this->lang->line('comments_email'); ?></label>
+			<div class="col-lg-10">
+		      <input name="email" id="email" type="email" value="<?php echo set_value('email'); ?>" class="form-control" placeholder="<?php echo lang('email'); ?>"  />
+              <?php if (form_error('email')) { echo form_error('email'); } else { ?>
+					<p class="help-block"><?php echo $this->lang->line('cat_form_name_help'); ?></p>
+			    <?php } ?>							
+			 </div>					  
 		  </div>
 
 		<?php else: ?>
 
 		<div class="form-group">
-		      <input name="nickname" id="nickname" type="text" value="<?php echo $this->ion_auth->get_display_name(); ?>" class="form-control" placeholder="<?php echo lang('nickname'); ?>" required disabled />
+		      <input name="nickname" id="nickname" type="text" value="<?php echo $this->ion_auth->get_display_name(); ?>" class="form-control" placeholder="<?php echo lang('nickname'); ?>" disabled />
 		  </div>
 
 
 		<?php endif ?>
 		  
 
-		  <div class="form-group">
-		      <textarea name="comment" id="comment" rows="6" cols="46" class="form-control" placeholder="<?php echo lang('comments_title'); ?>" required><?php echo set_value('comment'); ?></textarea>
+		  <div class="form-group<?php if (form_error('comment')) { ?> has-error has-feedback <?php } ?>">
+			<label for="comment" class="col-lg-2 control-label"><?php echo $this->lang->line('comments_title'); ?></label>
+			<div class="col-lg-10">
+		      <textarea name="comment" id="comment" rows="6" cols="46" class="form-control" placeholder="<?php echo lang('comments_title'); ?>"><?php echo set_value('comment'); ?></textarea>
+               <?php if (form_error('comment')) { echo form_error('comment'); } else { ?>
+					<p class="help-block"><?php echo $this->lang->line('comment_help_text'); ?></p>
+			    <?php } ?>							
+			 </div>				  
 		  </div>
-		  <div>
-		  	<p><small><em><?= lang('comment_help_text') ?></em></small></p>
-		  </div>
-
+ 
 		  <?php if ($this->config->item('use_recaptcha') == 1): ?>
 		  <div class="form-group">
-			  <script src='https://www.google.com/recaptcha/api.js'></script>
+			  <script type="text/javascript" src='https://www.google.com/recaptcha/api.js'></script>
 			  <div class="g-recaptcha" data-sitekey="<?php echo $this->config->item('recaptcha_site_key') ?>"></div>
 		  </div>
 			<?php endif ?>
@@ -121,6 +156,8 @@
 		  </div>
 		</form>
 
+		
+	</div>
 
 		<?php else: ?>
 			<?php if ($this->config->item('allow_comments') == '0'): ?>
@@ -129,11 +166,17 @@
 				<p><?php echo lang('comments_disabled_post'); ?></p>
 			<?php endif ?>
 		<?php endif; ?>
-
-	</div>
+ 
 </div>
 
+  
+  
+  
+  </div>
+</div>
+
+
 <?php else: ?>
-	<h2><?= lang('error_404_heading') ?></h2>
-	<p><?= lang('error_404_message') ?></p>
+	<h2><?php echo lang('error_404_heading'); ?></h2>
+	<p><?php echo lang('error_404_message'); ?></p>
 <?php endif ?>
