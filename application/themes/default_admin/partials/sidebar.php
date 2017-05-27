3df79041753bd5dc7bb39aa3157c380243cf3606
Simon <simon_montano@yahoo.com>
2017-05-27 16:45:48 -0400

    Add files via upload

--- application/themes/default_admin/partials/sidebar.php
+++ application/themes/default_admin/partials/sidebar.php
@@ -1,9 +1,10 @@
         <div class="col-sm-3 col-md-2 col-xs-12 sidebar">
           <ul class="nav nav-sidebar">
             <?php foreach ($this->template->admin_nav as $nav): ?>
-            <li class="<?php echo ($this->template->active_link == $nav['name'])? "active": ''; ?>"><?= $nav['link'] ?></li>
+            <li class="<?php echo ($this->template->active_link == $nav['name'])? "active": ''; ?>"><?php echo $nav['link']; ?></li>
             <?php endforeach ?>
- 
+           
           </ul>
- 
+
+
         </div>
