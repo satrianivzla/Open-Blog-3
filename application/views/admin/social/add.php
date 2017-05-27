f3fc3efffc82b6e97973023b09fc3210e4389869
Simon <simon_montano@yahoo.com>
2017-05-27 16:32:12 -0400

    Add files via upload

--- application/views/admin/social/add.php
+++ application/views/admin/social/add.php
@@ -1,36 +1,85 @@
-<div class="container">
-    <div class="row">
-		<div class="col-md-4 col-md-offset-4">
-			<?= validation_errors() ?>
+<?php 
+ /**
+ * Adding a New Social Link
+ * 
+ * @access       public
+ * @author       Enliven Appications
+ * @version      3.0
+ * Last Updated  May 04, 2017
+ * used bootstrap panels, breadcrumbs and alerts; Changed way to show validation messages and also added some language variables 
+ * @author       Simon Montaño
+*/
+?>
+         <div class="btn-group btn-breadcrumb">
+            <a href="<?php echo site_url('admin'); ?>" class="btn btn-default"><i class="glyphicon glyphicon-home"></i></a>
+            <a href="<?php echo site_url('admin_social'); ?>" class="btn btn-default"><?php echo lang('social_section_name'); ?></a>
+			<a href="#" class="btn btn-primary"><?php echo lang('social_add_page'); ?></a> 
+         </div>		 
+		 <br /> &nbsp; <br /> 
+
+
+  <div class="row">
+		 
+        <?php 
+		   if (validation_errors()) {  ?>
+			<div class="alert alert-danger alert-dismissable">
+			  <button type="button" class="close" data-dismiss="alert">&times;</button>
+			  <strong><?php echo lang('error_title_important'); ?></strong> <br /> 
+			  <?php echo lang('error_general_message'); ?>
+			</div>
+         <?php } ?>
 
 			<?php if (isset($message)): ?>
 				<div class="alert alert-danger" role="alert">
-					<?= $message ?>
+					<?php echo $message; ?>
 				</div>
 			<?php endif ?>
+			
+			
     		<div class="panel panel-default">
 			  	<div class="panel-heading">
-			    	<h3 class="panel-title"><?= lang('index_add_new_social');?></h3>
+			    	<h3 class="panel-title"><?php echo lang('index_add_new_social'); ?></h3>
 			 	</div>
 			  	<div class="panel-body">
-			    	<?= form_open(current_url());?>
-			    		<p><?= lang('add_social_subheading');?></p>
+			    	<?php echo form_open(current_url()); ?>
+			    		<div class="well"><?php echo lang('add_social_subheading'); ?></div>
                     <fieldset>
-			    	  	<div class="form-group">
-			    		    <?= form_input(['name'=> 'name', 'id' => 'name', 'class' => "form-control", 'placeholder' => lang('social_form_name'), 'value' => set_value('name')]) ?>
+			    	  	<div class="form-group<?php if (form_error('name')) { ?> has-error has-feedback <?php } ?>">
+						<label for="name" class="col-lg-2 control-label"><?php echo $this->lang->line('social_form_name'); ?></label>
+						<div class="col-lg-10">
+			    		    <?php echo form_input(['name'=> 'name', 'id' => 'name', 'class' => "form-control", 'placeholder' => lang('social_form_name'), 'value' => set_value('name')]); ?>
+			    		    <?php if (form_error('name')) { echo form_error('name'); } else { ?>
+								<p class="help-block"><?php echo $this->lang->line('social_form_name'); ?></p>
+							<?php } ?>							
 			    		</div>
-			    		<div class="form-group">
-			    			<?= form_input(['name'=> 'url', 'id' => 'url', 'class' => "form-control", 'placeholder' => lang('social_form_url'), 'value' => set_value('url')]) ?>
+						</div>						
+						
+			    		<div class="form-group<?php if (form_error('url')) { ?> has-error has-feedback <?php } ?>">
+						<label for="url" class="col-lg-2 control-label"><?php echo $this->lang->line('social_form_url'); ?></label>
+						<div class="col-lg-10">
+			    			<?php echo form_input(['name'=> 'url', 'id' => 'url', 'class' => "form-control", 'placeholder' => lang('social_form_url'), 'value' => set_value('url')]); ?>
+                            <?php if (form_error('name')) { echo form_error('url'); } else { ?>
+								<p class="help-block"><?php echo $this->lang->line('social_form_url'); ?></p>
+							<?php } ?>							
+			    		</div>							
 			    		</div>
-			    		<div class="form-group">
-			    			<label><?= lang('social_form_active') ?></label>
-			    			<?= form_dropdown('enabled', ['0' => lang('no'), '1' => lang('yes')], '1', 'class="form-control"') ?>
+						
+			    		<div class="form-group<?php if (form_error('enabled')) { ?> has-error has-feedback <?php } ?>">
+						<label for="enabled" class="col-lg-2 control-label"><?php echo $this->lang->line('social_form_status'); ?></label>
+						<div class="col-lg-10">			    			 
+			    			<?php							 
+							echo form_dropdown('enabled', ['0' => lang('no'), '1' => lang('yes')], '1', 'id="enabled" class="form-control"'); ?>
+                            <?php if (form_error('enabled')) { echo form_error('enabled'); } else { ?>
+								<p class="help-block"><?php echo $this->lang->line('social_form_enabled'); ?></p>
+							<?php } ?>							
+			    		</div>								
 			    		</div>
-			    		<input class="btn btn-lg btn-default btn-block" type="submit" value="<?php echo lang('save_social_btn');?>">
+							<br /> &nbsp; <br />					
+						
+			    		<input class="btn btn-lg btn-default btn-block" type="submit" value="<?php echo lang('save_social_btn'); ?>">
 			    	</fieldset>
 			      	</form>
 			    </div>
 			</div>
 		</div>
-	</div>
-</div>
+ 
--- application/views/admin/social/index.php
+++ application/views/admin/social/index.php
@@ -1,23 +1,82 @@
-<h2><?= lang('social_hdr') ?></h2>
-<p><?= lang('social_hdr_help_txt') ?></p>
-<p><a class="btn btn-default btn-sm" href="<?php echo site_url('admin_social/add') ?>"><?php echo lang('social_add_new') ?></a></p>
+<?php 
+ /**
+ * Social Links
+ * 
+ * @access       public
+ * @author       Enliven Appications
+ * @version      3.0
+ * Last Updated  May 04, 2017
+ * used bootstrap panels, breadcrumbs and alerts; Changed way to show validation messages and also added some language variables 
+ * @author       Simon Montaño
+*/
+?> 
+         <div class="btn-group btn-breadcrumb">
+            <a href="<?php echo site_url('admin'); ?>" class="btn btn-default"><i class="glyphicon glyphicon-home"></i></a>
+            <a href="<?php echo site_url('admin_social'); ?>" class="btn btn-default"><?php echo lang('social_section_name'); ?></a> 
+			<a href="#" class="btn btn-primary">Index</a>
+         </div>		 
+		 <br /> &nbsp; <br /> 
+		 
+ <div class="text-right"><a class="btn btn-default btn-sm" href="<?php echo site_url('admin_social/add'); ?>"><?php echo lang('social_add_new'); ?> <i class="fa fa-plus" aria-hidden="true"></i></a> </div>
+ <br />
 
-<table class="table table-condensed">
-    <tr>
-        <th>Name</th>
-        <th>URL</th>
-        <th>Enabled</th>
-        <th></th>
-    </tr>
-    <?php foreach ($social as $item): ?>
-        <tr>
-        <td><?= $item['name'] ?></td>
-        <td><a href="<?= $item['url'] ?>" target="_blank"><?= $item['url'] ?></a></td>
-        <td><?php echo ($item['enabled'] == '1') ? lang('yes') : lang('no'); ?></td>
-        <td class="text-right">
-            <a href="<?= site_url('admin_social/edit/' . $item['id']) ?>" class="btn btn-default btn-xs"><?= lang('social_edit_btn') ?></a>
-            <a href="<?= site_url('admin_social/remove/' . $item['id']) ?>" class="btn btn-danger btn-xs"><?= lang('social_remove_btn') ?></a>
-        </td>
-    </tr>
-  <?php endforeach ?>
-</table>
+		<div class="panel panel-default">
+		  <div class="panel-heading">
+			<h3 class="panel-title"><?php echo lang('social_hdr'); ?></h3>
+		  </div>
+		  <div class="panel-body">
+		  
+		  <div class="well"><?php echo lang('social_hdr_help_txt'); ?></div>
+
+		  <?php
+				 
+				if (empty($social)) { ?>   
+
+				<div class="alert alert-warning alert-dismissable">
+				  <button type="button" class="close" data-dismiss="alert">&times;</button>
+				  <strong><?php echo lang('error_title_important'); ?></strong>
+				  <br /> 
+				  <?php echo lang('error_no_social_links'); ?>
+				</div>
+				  
+				<?php } else  { ?>
+
+               <table id="listing" class="table table-striped table-bordered">
+                  <thead>
+					<tr>
+						<th><?php echo lang('title_name'); ?></th>
+						<th><?php echo lang('social_form_url'); ?></th>
+						<th><?php echo lang('enabled'); ?></th>
+						<th><?php echo lang('title_edit'); ?></th>
+						<th><?php echo lang('title_delete'); ?></th>  
+					</tr>
+				</thead>
+                <tbody>	
+					<?php foreach ($social as $item): ?>
+						<tr>
+						<td><?php echo $item['name']; ?></td>
+						<td><a href="<?php echo $item['url']; ?>" target="_blank"><?php echo $item['url']; ?></a></td>
+						<td><?php echo ($item['enabled'] == '1') ? lang('yes') : lang('no'); ?></td>
+						<td>
+							<a href="<?php echo site_url('admin_social/edit/' . $item['id']); ?>" class="btn btn-default btn-xs"><i class="fa fa-pencil" aria-hidden="true"></i> <?php echo lang('social_edit_btn'); ?></a>
+						</td>
+						<td><a href="<?php echo site_url('admin_social/remove/' . $item['id']); ?>" class="btn btn-danger btn-xs"><i class="fa fa-trash" aria-hidden="true"></i> <?php echo lang('social_delete_btn'); ?></a>
+						</td>
+					</tr>
+				  <?php endforeach ?>
+                  </tbody>		
+                    <tfoot>
+                        <tr>
+						<th><?php echo lang('title_name'); ?></th>
+						<th><?php echo lang('social_form_url'); ?></th>
+						<th><?php echo lang('enabled'); ?></th>
+						<th><?php echo lang('title_edit'); ?></th>
+						<th><?php echo lang('title_delete'); ?></th>  
+					</tr>
+                    </tfoot>
+            </table> 
+				<?php } ?>		  
+						  
+		  
+		  </div>
+		</div>
